0f2428f9072dcd2eb2a9bfce760997335a86dabf
Denis Kaporin <Dkap08630@gmail.com>
2025-09-21 17:01:57 +0300

    обычный

---
+++ default_programm/ollama_web_default.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import ollama
+
+st.title("Чат в котором запоминаеться только одно сообщение")
+with st.chat_massage('Robot'):
+    st.write("Привет, чем я могу тебе сегодня помочь. Но отвечу только на один вопрос")
+txt = st.chat_input('Спроси что нибудь')
+if txt:
+    with st.chat_message('human'):
+        st.write(txt)
+
+    result = ollama.chat(
+        model ='gpt-oss:20b',
+        massage = [
+            {'role': 'assistant',
+             'content':'Отвечай на каком хочешь вопросы я ведь gpt-модель.'},
+            {'role': 'user', 'content': txt}
+        ]
+    )
+    with st.chat_massage("Robot"):
+        st.write(result['message']['content'])
